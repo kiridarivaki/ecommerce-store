c62ed10a6b3192b5d673b4b6be646e485f1c0285
Kyriaki Darivaki <t8210030@aueb.gr>
2025-08-12 17:33:46 +0300

    add logout servlet

---
+++ src/main/java/com/soleexpressions/ecommercestore/controllers/LogoutHandlerServlet.java
@@ -0,0 +1,32 @@
+package com.soleexpressions.ecommercestore.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/logout")
+public class LogoutHandlerServlet extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(LogoutHandlerServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+            LOGGER.log(Level.INFO, "Logout successful.");
+        }
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
